c51f775e097623d49ff62d22e5ac25e5f785c99d
arisbhakti <arisscalper@gmail.com>
2025-12-21 13:35:41 +0700

    adding comment in header

--- src/components/container/Header/Header.tsx
+++ src/components/container/Header/Header.tsx
@@ -9,7 +9,6 @@ import BrandLogo from "../../ui/Button/BrandLogo";
 export default function Header() {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
-  // Optional tapi enak: kalau pindah ke desktop, tutup menu mobile
   useEffect(() => {
     const onResize = () => {
       if (window.innerWidth >= 768) setIsMobileMenuOpen(false);
@@ -28,10 +27,13 @@ export default function Header() {
   return (
     <>
       <header className="w-full max-w-[1440px] fixed h-16 md:h-21 backdrop-blur-2xl flex justify-between items-center py-6 px-4 md:px-35 z-50">
+        {/* YOUR LOGO */}
         <BrandLogo onClose={() => setIsMobileMenuOpen(false)}></BrandLogo>
 
+        {/* Navigation (About , Services , Projects, Testimonials, FAQ ) */}
         <DesktopNav items={desktopItems} />
 
+        {/* Button "Let's Talk" */}
         <ButtonPrimary
           className="hidden md:flex font-medium"
           onClick={() => {
@@ -43,12 +45,14 @@ export default function Header() {
           Let's Talk
         </ButtonPrimary>
 
+        {/* Button Humberger (only displayed in mobile view) */}
         <MobileMenuToggle
           open={isMobileMenuOpen}
           onToggle={() => setIsMobileMenuOpen((v) => !v)}
         />
       </header>
 
+      {/* Mobile Menu (only displayed in mobile view) */}
       <MobileMenu
         isOpen={isMobileMenuOpen}
         onClose={() => setIsMobileMenuOpen(false)}
